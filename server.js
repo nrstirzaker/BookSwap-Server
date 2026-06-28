e4e7cc74b2d50d7d64f4d3e0f6d3d60fa84ef8b1
taros <thelu@TOMS-SURFACE>
2026-06-28 14:34:58 +0100

    Added PostGreSQL to the project.

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import express from "express";
+import * as pgp from "./node_modules/pg-promise/typescript/pg-promise";
+
 const app = express();
+const port = 3000;
+
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Listening to port ${port}...`)
+});
